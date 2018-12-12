f60061d65c82942743032d2d5d0e78f346a35ea8
Francois Monette <fmonett@videotron.ca>
2018-12-11 19:35:52 -0500

    Fixed error

--- 11-2.py
+++ 11-2.py
@@ -5,8 +5,8 @@ grid = [[0 for i in range(0,300)] for j in range(0,300)]
 satgrid = [[0 for i in range(0,300)] for j in range(0,300)]
 serial=5093
 
-for y in range(1,300):
-    for x in range(1,300):
+for y in range(0,300):
+    for x in range(0,300):
         rackid = x + 10
         power = rackid * y
         power += serial
@@ -17,7 +17,7 @@ for y in range(1,300):
         else:
             power = int(powerstr[-3]) - 5
         
-        satgrid[y][x] = power + (0 if y == 1 else satgrid[y-1][x]) + (0 if x == 1 else satgrid[y][x-1]) - (0 if x == 1 and y== 1 else satgrid[y-1][x-1])
+        satgrid[y][x] = power + (0 if y == 0 else satgrid[y-1][x]) + (0 if x == 0 else satgrid[y][x-1]) - (0 if x == 0 and y== 0 else satgrid[y-1][x-1])
         grid[y][x] = power
         
 maxpower = -1
